9785f9dfa91f161c5dda0a0b1ce34a44a2076938
Michael Stiber <stiber@u.washington.edu>
2026-05-25 11:09:50 -0700

    Added missing symbols

--- mini_torch/__init__.py
+++ mini_torch/__init__.py
@@ -10,7 +10,8 @@ from .Dataset import Dataset
 from .Loss import Loss
 from .Module import Module
 from .Optimizer import Optimizer
-from .backend import xp, is_gpu_available, asnumpy, as_backend_array
+from .backend import xp, is_gpu_available, asnumpy, as_backend_array, scipy_special, scipy_signal
+
 
 __all__ = [
     "Activation",
@@ -23,4 +24,6 @@ __all__ = [
     "is_gpu_available",
     "asnumpy",
     "as_backend_array",
+    "scipy_special",
+    "scipy_signal"
 ]
